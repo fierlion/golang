c44c6315ae82ae19b647efd62e3d7b2c4fbd7e1a
fierlion <fierlion@gmail.com>
2016-06-10 13:25:29 -0700

    implementing extra bitset methods: remove() clear() copy()

--- src/bitset/bitset.go
+++ src/bitset/bitset.go
@@ -5,29 +5,44 @@ import (
 	"fmt"
 )
 
-//an IntSet is a set of small non-negative integers.
+//an BitSet is a set of small non-negative integers.
 //its zero value represents the empty set.
-type IntSet struct {
+type BitSet struct {
 	words []uint64
+	size int
 }
 
 //Has reports whether the set containst the non-negative value x
-func (s *IntSet) Has(x int) bool {
+func (s *BitSet) Has(x int) bool {
 	word, bit := x/64, uint(x%64)
 	return word < len(s.words) && s.words[word]&(1<<bit) != 0
 }
 
 //Add adds the non-negative value x to the set
-func (s *IntSet) Add(x int) {
+func (s *BitSet) Add(x int) {
 	word, bit := x/64, uint(x%64)
 	for word >= len(s.words) {
 		s.words = append(s.words, 0)
 	}
 	s.words[word] |= 1 << bit
+	s.size += 1
+}
+
+//Remove clears the non-negative value x from the set
+func (s *BitSet) Remove(x int) {
+	word, bit := x/64, uint(x%64)
+	s.words[word] &= ^(1<< bit)
+	s.size -= 1
+}
+
+//Clear sets the bitset to 0
+func (s *BitSet) Clear() {
+	s.words = make([]uint64, 0)
+	s.size = 0
 }
 
 //UnionWith sets s to the union of s and t.
-func (s *IntSet) UnionWith(t *IntSet) {
+func (s *BitSet) UnionWith(t *BitSet) {
 	for i, tword := range t.words {
 		if i < len(s.words) {
 			s.words[i] |= tword
@@ -37,8 +52,19 @@ func (s *IntSet) UnionWith(t *IntSet) {
 	}
 }
 
+//Len returns the size of the bitset
+func (s *BitSet) Len() int {
+	return s.size
+}
+
+//Copy returns a deep copy of the BitSet
+func (s *BitSet) Copy() *BitSet {
+  cpy := BitSet{s.words, s.size}
+	return &cpy
+}
+
 //String returns the set as a string of the form "{1 2 3}"
-func (s *IntSet) String() string {
+func (s *BitSet) String() string {
 	var buf bytes.Buffer
 	buf.WriteByte('{')
 	for i, word := range s.words {
--- src/methods/methods.go
+++ src/methods/methods.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"geometry"
+	"bitset"
 	"fmt"
 )
 
@@ -12,4 +13,29 @@ func main() {
 	pointp := &p
 	pointp.ScaleBy(50)
 	fmt.Printf("%f, %f\n", p.X, p.Y)
+
+	var x, y bitset.BitSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	fmt.Println(x.String())
+	y.Add(9)
+	y.Add(42)
+	fmt.Println(y.String())
+	x.UnionWith(&y)
+	fmt.Println(x.String())
+	fmt.Println(x.Has(9), x.Has(123))
+	x.Remove(9)
+	fmt.Println(x.Len())
+	fmt.Println(x.String())
+  x.Clear()
+	fmt.Println(x.String())
+	fmt.Println(x.Len())
+	x.Add(9)
+	fmt.Println(x.String())
+	fmt.Println(x.Len())
+	cpy := x.Copy()
+	cpy.Add(100)
+	fmt.Println(x.String())
+	fmt.Println(cpy.String())
 }
